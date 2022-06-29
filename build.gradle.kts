fb33fe11b15867d485111f926c2904721bacdffb
白叶 藤原 <1751842477@qq.com>
2022-06-29 19:44:37 +0800

    BugFix

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,6 @@ repositories {
 }
 dependencies {
     implementation("com.tencentcloudapi:tencentcloud-sdk-java:4.0.11")
-    implementation("com.alibaba:fastjson:2.0.3")
+    implementation("com.alibaba:fastjson:2.0.7")
     implementation("org.xerial:sqlite-jdbc:3.36.0.3")
 }
